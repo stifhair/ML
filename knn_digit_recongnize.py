ce10f3ad667cf00719a47738cd8d60234eebcc38
junghwan <stifhair0@gmail.com>
2020-05-28 17:29:19 +0900

    commit

--- knn_digit_recongnize.py
+++ knn_digit_recongnize.py
@@ -8,6 +8,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # 세로로 50줄, 가로로 100줄로 사진을 나눈다.
 cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
 x     = np.array(cells)
+print(x.shape)
 
 # 각 (20 X 20) 크기의 사진을 한 줄(1 X 400)으로 변환한다.
 train =  x[:, :].reshape(-1, 400).astype(np.float32)
