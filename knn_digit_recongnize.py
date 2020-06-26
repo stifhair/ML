aad7255e45706af2c56baec091d27c2c443d24fb
stifhair <stifhair0@gmail.com>
2020-06-26 09:19:32 +0900

    commit

--- knn_digit_recongnize.py
+++ knn_digit_recongnize.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 img  = cv2.imread('digits.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -17,3 +18,18 @@ train =  x[:, :].reshape(-1, 400).astype(np.float32)
 k = np.arange(10)
 train_labels = np.repeat(k,500)[:, np.newaxis]
 np.savez("trained.npz", train=train, train_labels=train_labels)
+
+plt.imshow(cv2.cvtColor(x[0, 0], cv2.COLOR_GRAY2RGB))
+plt.show()
+
+# 다음과 같이 하나씩 글자를 저장할 수 있습니다.
+cv2.imwrite('test_0.png', x[0, 0])
+cv2.imwrite('test_1.png', x[5, 0])
+cv2.imwrite('test_2.png', x[10, 0])
+cv2.imwrite('test_3.png', x[15, 0])
+cv2.imwrite('test_4.png', x[20, 0])
+cv2.imwrite('test_5.png', x[25, 0])
+cv2.imwrite('test_6.png', x[30, 0])
+cv2.imwrite('test_7.png', x[35, 0])
+cv2.imwrite('test_8.png', x[40, 0])
+cv2.imwrite('test_9.png', x[45, 0])
